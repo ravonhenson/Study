3bd4ece9e4f3c94475099c4c69646c36deb10a5d
ravonhenson <ravonhenson@gmail.com>
2023-11-26 17:05:37 -0500

    Initial Commit

---
+++ Study/StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyApp.swift
+//  Study
+//
+//  Created by Ravon Henson on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
